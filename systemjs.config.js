f5009d82415aef0c2df8c3c4f7d2ee93f9679a22
Jim Cooper <jim@cooperplanet.com>
2016-07-18 15:12:20 -0600

    Remove in-memory api from systemjs config

--- systemjs.config.js
+++ systemjs.config.js
@@ -14,7 +14,6 @@
   var packages = {
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
   var ngPackageNames = [
     'common',

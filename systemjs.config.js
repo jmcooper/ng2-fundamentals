f7da9175cc917138985b7916bb1accc6df782d11
Jim Cooper <jim@cooperplanet.com>
2016-07-23 12:22:00 -0600

    Finish removing in-memory api from systemjs config

--- systemjs.config.js
+++ systemjs.config.js
@@ -7,7 +7,6 @@
   var map = {
     'app':                        'app', // 'dist',
     '@angular':                   'node_modules/@angular',
-    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs'
   };
   // packages tells the System loader how to load when no filename and/or no extension

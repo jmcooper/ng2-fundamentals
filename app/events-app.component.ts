1e11c0b93a8f7b77c0a9d9a812b606854c7f3382
Jim Cooper <jim@cooperplanet.com>
2016-07-23 14:50:44 -0600

    Hello World - need to add app SystemJS package

---
+++ app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+		selector: 'events-app',
+        template: '<h2>Hello World</h2>'
+	})
+export class EventsAppComponent {
+  constructor() {}
+}

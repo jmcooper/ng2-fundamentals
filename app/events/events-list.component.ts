14156060a882201c9209e5ac71fb77b8e8e12806
Jim Cooper <jim@cooperplanet.com>
2016-07-25 17:19:29 -0600

    Fix <hr/>

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -7,17 +7,17 @@ import { Component } from '@angular/core';
     <h1>Upcoming Angular 2 Events</h1>
     <br>
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <span>Date:</span>
-        <span>{{event.date}}</span><br>
-        <span>Time:</span>
-        <span>{{event.time}}</span><br>
-        <span>Price:</span>
-        <span>\${{event.price}}</span><br>
-        <span>Location:</span>
-        <span>{{event.location.address}}</span>
-        <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
+      <h2>{{event.name}}</h2>
+      <span>Date:</span>
+      <span>{{event.date}}</span><br>
+      <span>Time:</span>
+      <span>{{event.time}}</span><br>
+      <span>Price:</span>
+      <span>\${{event.price}}</span><br>
+      <span>Location:</span>
+      <span>{{event.location.address}}</span>
+      <span>&nbsp;</span>
+      <span>{{event.location.city}}, {{event.location.country}}</span>
     </div>
   </div>
   `

db890cf96235f4553aad8343b486e05fbaa5f80b
Jim Cooper <jim@cooperplanet.com>
2016-07-25 16:41:48 -0600

    Data-binding to event

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { EventsListComponent } from './events/events-list.component';
 
 @Component({
   selector: 'events-app',
-  template: '<h2>Hello World</h2>'
+  template: '<events-list></events-list>',
+  directives: [EventsListComponent]
 })
 export class EventsAppComponent {
   constructor() {}

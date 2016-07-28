62fe7e6a8cb115ece6b8930729f55a1152125b39
Jim Cooper <jim@cooperplanet.com>
2016-07-27 21:29:30 -0600

    Basic styling

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -3,7 +3,12 @@ import { EventsListComponent } from './events/events-list.component';
 
 @Component({
   selector: 'events-app',
-  template: '<events-list></events-list>',
+  template: `
+  	<div class="well">
+      <div>Hello World</div>
+    </div>
+    <events-list></events-list>
+  `,
   directives: [EventsListComponent]
 })
 export class EventsAppComponent {

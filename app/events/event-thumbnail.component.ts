01193df24c24f9c0511f3f49107a6b478630eba1
Jim Cooper <jim@cooperplanet.com>
2016-08-08 18:57:10 -0600

    Safe Navigator Operator (?)

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,18 +4,22 @@ import { Component, Input, Output, EventEmitter  } from '@angular/core';
   selector: 'event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail" (click)="handleClickMe()">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
+      <h2>{{event?.name}}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div>Time: {{event?.time}}</div>
+      <div>Price: \${{event?.price}}</div>
       <div>
-        <span>Location: {{event.location.address}}</span>
-        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <span>Location: {{event?.location?.address}}</span>
+        <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+      </div>
+      <div>
+        Online Url: {{event.onlineUrl}}
       </div>
     </div>
   `,
   styles: [`
     .pad-left { margin-left: 10px; }
+    .thumbnail { min-height: 210px; }
     .well div { color: #bbb }
   `]
 })
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -102,11 +102,8 @@ export class EventsListComponent {
       time: '9:00 am',
       price: 950.00,
       imageUrl: '/app/assets/images/ng-nl.png',
-      location: {
-        address: 'The NG-NL Convention Center & Scuba Shop',
-        city: 'Amsterdam',
-        country: 'Netherlands'
-      },
+      onlineUrl: 'http://ng-nl.org/'
+      ,
       sessions: [
         {
           id: 1,

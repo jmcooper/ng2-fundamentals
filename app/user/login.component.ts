cb922ed2e55ada58b0a53a515b61c883f50dbad0
Jim Cooper <jim@cooperplanet.com>
2016-09-09 01:01:43 -0600

    Add validation to login form

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service'
+import { Router } from '@angular/router'
 
 @Component({
-  templateUrl: 'app/user/login.component.html' 
+  templateUrl: 'app/user/login.component.html',
+  styles: [`
+    em {float:right; color:#E05C65; padding-left:10px;}
+  `]
 })
 export class LoginComponent {
-  constructor(private authService:AuthService) {
+  constructor(private authService:AuthService, private router:Router) {
 
   }
 
   login(formValues) { 
     this.authService.loginUser(formValues.userName, formValues.password)
+    this.router.navigate(['events'])
   } 
+
+  cancel() {
+    this.router.navigate(['events'])
+  }
 
 }

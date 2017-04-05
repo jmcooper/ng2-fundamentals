4bdeafd0b11fba40961cf341dbaa9c1c934c52ac
juliuzy <juliusb@zylun.com>
2017-04-06 04:10:11 +0800

    *ngFor

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -16,8 +16,10 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
             </div>
     `,
     styles:[`
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left:10px; }
         .well div { color: #bbb; }
+
     `]
 })
 export class EventThumbnailComponent{

7d655c3d120f5fa4d89c9b1075ca928c4bd99f45
juliuzy <juliusb@zylun.com>
2017-04-06 04:20:34 +0800

    Show/HIde ngIf

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,13 +4,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     selector: 'event-thumbnail',
     template: `
         <div class="well hoverwell thumbnail">
-                <h2>{{event.name}}</h2>
-                <div>Date: {{event.date}}</div>
-                <div>Time: {{event.time}}</div>
-                <div>Price: \${{event.price}}</div>
-                <div>
-                    <span>Location: {{event.location.address}}</span>                    
-                    <span class="pad-left">{{event.location.city}},{{event.location.country}}</span>
+                <h2>{{event?.name}}</h2>
+                <div>Date: {{event?.date}}</div>
+                <div>Time: {{event?.time}}</div>
+                <div>Price: \${{event?.price}}</div>
+                
+                <div *ngIf="event?.location">
+                    <span>Location: {{event?.location?.address}}</span>                    
+                    <span class="pad-left">{{event?.location?.city}},{{event?.location?.country}}</span>
                 </div>
                 
             </div>

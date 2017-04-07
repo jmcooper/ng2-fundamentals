66bdf088500c684f8c99b31dce77c373f40ac59f
juliuzy <juliusb@zylun.com>
2017-04-08 00:12:00 +0800

    styling active links

--- app/nav/navbar.component.ts
+++ app/nav/navbar.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core'
         .nav.navbar-nav {font-size:15px;}
         #searchForm {margin-right:100px;}
         @media (max-width:1200px) {#searchForm {display:none }}
+        li > a.active { color:#F97924; }
     `]
 })
 export class NavbarComponent{

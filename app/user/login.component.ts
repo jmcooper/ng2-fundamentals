ac04944a246e46c6a1b058696119e3647681e7de
juliuzy <juliusb@zylun.com>
2017-04-08 02:20:51 +0800

    Validating Template Base Form

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core'
 import { AuthService } from './auth.service'
 import { Router } from '@angular/router'
 @Component({
-    templateUrl: 'app/user/login.component.html'
+    templateUrl: 'app/user/login.component.html',
+    styles:[`
+        em { float:right; color:#E05C65; padding-left:10px; }
+    `]
 })
 export class LoginComponent{
     constructor(private authService: AuthService, private router: Router){

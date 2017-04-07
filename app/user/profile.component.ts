0800458778e744393f4088291f78fa7b0dc03d19
juliuzy <juliusb@zylun.com>
2017-04-08 03:26:40 +0800

    multiple validators

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -24,7 +24,8 @@ export class ProfileComponent implements OnInit {
      }
 
      ngOnInit()  {
-       this.firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
+       this.firstName = new FormControl(this.authService.currentUser.firstName, 
+                              [Validators.required,Validators.pattern('[a-zA-Z].*')])
        this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
        this.profileForm = new FormGroup({
            firstName: this.firstName,

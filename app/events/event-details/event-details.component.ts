567119ff1a020e347150ee8b4b410c51d76eedb4
juliuzy <juliusb@zylun.com>
2017-04-18 02:45:28 +0800

    Sorting Data

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -15,6 +15,7 @@ export class EventDetailsComponent implements OnInit{
     event:IEvent
     addMode:boolean
     filterBy: string = 'all';
+    sortBy: string = 'votes';
     constructor(private eventService:EventService,
         private route:ActivatedRoute){
 
--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -8,11 +8,13 @@ import { ISession } from '../shared/index'
 export class SessionListComponent implements OnChanges{
     @Input() sessions:ISession[];
     @Input() filterBy:string;
+    @Input() sortBy:string;
     visibleSessions:ISession[] = [];
 
     ngOnChanges(){
         if(this.sessions){
             this.filterSessions(this.filterBy);
+            this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc)
         }
     }
 
@@ -25,4 +27,14 @@ export class SessionListComponent implements OnChanges{
             })
         }
     }
+}
+
+function sortByNameAsc(s1:ISession, s2:ISession) {
+    if(s1.name > s2.name) return 1
+    else if(s1.name === s2.name) return 0
+    else return -1
+}
+
+function sortByVotesDesc(s1:ISession, s2: ISession) {
+    return s2.voters.length - s1.voters.length
 }

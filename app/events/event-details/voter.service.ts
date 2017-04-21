2c9b4871a24d1f39330988e8eba0644f8586af51
juliuzy <juliusb@zylun.com>
2017-04-22 02:13:40 +0800

    mocking testing services

--- app/events/event-details/voter.service.ts
+++ app/events/event-details/voter.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 import { ISession } from '../shared/event.model'
 import { Http, Response, Headers, RequestOptions } from '@angular/http'
-import { Observable } from 'rxjs/RX'
+import { Observable } from 'rxjs/Rx'
 @Injectable()
 export class VoterService{
 
